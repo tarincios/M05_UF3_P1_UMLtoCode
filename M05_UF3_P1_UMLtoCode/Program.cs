80de77efad7f311578de37bc4680e25be5f53b41
tarincios_HP21 <tarincios@gmail.com>
2023-04-16 11:55:54 +0200

    Agregar archivos de proyecto.

---
+++ M05_UF3_P1_UMLtoCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M05_UF3_P1_UMLtoCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
